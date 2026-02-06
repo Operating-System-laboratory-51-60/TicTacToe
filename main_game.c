ebc88d7c22881d34d41c8cffa8fa55e04d5edcae
Suvam Sarkar <suvammm21@gmail.com>
2026-02-07 00:55:37 +0530

    feat: Added the game loop and the validation for invalid moves

--- main_game.c
+++ main_game.c
@@ -13,3 +13,107 @@
 volatile int signal_count = 0; //
 volatile int move_received = 0; // Flag to indicate a full move has arrived
 char board[9] = {'1','2','3','4','5','6','7','8','9'}; // The board which we will modify as the game goes on
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int moves_count = 0;
+while(moves_count < 9)
+{
+    // print the board every 9 iteration
+    print_board();
+
+    if(check_win()) break; // is current board state is a winning state then break
+
+    if(my_turn)
+    {
+        int move;
+        printf("Your Turn (%c). Enter position (1-9): ", my_symbol);
+        scanf("%d", &move);
+        int idx = move - 1; // moves are 1 indexed and array is 0 indexed
+
+        if (idx < 0 || idx > 8 || board[idx] == 'X' || board[idx] == 'O')
+        {
+            // These are all invalid checks
+            printf("Invalid move!\n");
+            sleep(2);
+            continue;
+        }
